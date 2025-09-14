8fa51e254957406954f631cf2aa2365c04dbbe9d
lironshustin1996 <lironshustin1996@gmail.com>
2025-09-15 00:17:50 +0300

    Added bubble sort functions

---
+++ Plague_HIT/sort.h
@@ -0,0 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+#include "world.h"
+
+typedef struct AffectedRegions {
+	char name[50];
+	int affected_percentage;
+}AffectedRegions;
+
+int RegionAffectedPopulation(Regions* current_region);
+AffectedRegions* SortAffectedRegions(Regions* world_regions);
+void SaveToFile(AffectedRegions* affected_array);
+
+#endif 
+#pragma once
