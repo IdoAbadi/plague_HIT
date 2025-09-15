7861be7c9b80e2729f80b337472edbc326968bad
lironshustin1996 <lironshustin1996@gmail.com>
2025-09-15 03:55:44 +0300

    Added design functions (coloring and clear console) also added a welcoming message

---
+++ Plague_HIT/design.h
@@ -0,0 +1,19 @@
+#ifndef design_H
+#define design_H
+
+const char* RED;
+const char* YELLOW;
+const char* ORANGE;
+const char* BLUE;
+const char* GREEN;
+const char* CYAN;
+const char* PURPLE;
+const char* PINK;
+const char* RESET;
+
+void print_colored(const char* text, const char* color_code);
+void print_welcome_message();
+void clear_console();
+
+#endif
+#pragma once
